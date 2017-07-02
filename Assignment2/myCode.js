801c971b21b7092c66474e709d53611cd7d9510d
Doluhboi <ktb@uoregon.edu>
2017-07-02 16:01:33 -0700

    update

---
+++ Assignment2/myCode.js
@@ -0,0 +1,124 @@
+window.onload = function() {
+
+	/* On page load, the following will be executed
+	set clock value
+	generate two random avatar pictures
+	ask users for names
+	*/
+
+	// clock
+	setTimeout(updateTime, 500);
+	// let now = new Date();
+	// let ampm = "AM";
+
+	// h = now.getHours();
+	// m = now.getMinutes();
+	// s = now.getSeconds();
+	
+	// if(h>= 12){
+	// 	if(h>12) h -= 12;
+	// 	ampm= 'PM';
+	// }
+
+	// if(m<10) m= '0'+m;
+	// if(s<10) s= '0'+s;
+	// let dateTime = now.toLocaleDateString()+ ' ' + h + ':' + m + ':' + s + ' ' + ampm;
+	// let t = document.querySelector("#time");
+	// let tVal = document.createElement("span");
+	// tVal.innerHTML = dateTime;
+	// t.appendChild(tVal);
+
+	//avatars
+	let avatars = ["/home/kyle/CIS-115/Assignment1/images/iq.jpg",
+	"/home/kyle/CIS-115/Assignment1/images/sledge.jpg","/home/kyle/CIS-115/Assignment1/images/jager.jpg",
+	"/home/kyle/CIS-115/Assignment1/images/castle.jpg","/home/kyle/CIS-115/Assignment1/images/fuze.jpg"];
+	let rand1 = avatars[Math.floor(Math.random() * avatars.length)];
+	let rand2 = avatars[Math.floor(Math.random() * avatars.length)];
+	while (rand1 == rand2){
+		rand2 = avatars[Math.floor(Math.random() * avatars.length)];
+	}
+	let avatar1 = document.querySelector("#avatar1");
+	avatar1.src = rand1;
+	let avatar2 = document.querySelector("#avatar2");
+	avatar2.src = rand2;
+
+
+	//get users info
+	//let userName = prompt("User 1 name: ");
+	let userName = "kyle"; 					// for dev
+	let ul = document.querySelector("#user1");
+	let user1Id = document.createElement("ul");
+	user1Id.innerHTML = 'Player 1: '+userName.toUpperCase();
+	ul.appendChild(user1Id);
+
+	//userName = prompt("User 2 name: ");
+	userName = "batman"   	// for dev
+	ul = document.querySelector("#user2");
+	let user2Id = document.createElement("ul");
+	user2Id.innerHTML = 'Player 2: '+userName.toUpperCase();
+	ul.appendChild(user2Id);
+
+
+	// create a object 'code'
+	let obj = {};
+	for(let i = 1; i<10;i++){
+		obj["GC_"+i] = "Level-"+i;
+	}
+
+	// add button feature
+	let btn = document.querySelector("#gcBtn");
+	btn.addEventListener("click", addGC);
+
+	// reset info button
+	let btn2 = document.querySelector("#confirmBtn");
+	btn2.addEventListener("click", confirmInfo);
+
+	let btn3 = document.querySelector("#resetBtn");
+	btn3.addEventListener("click", resetInfo);
+
+
+}
+
+function addGC(){
+	let opt = document.querySelector("#lvl");
+	let input = document.querySelector("#gcInput");
+	let newOpt = document.createElement("option");
+	newOpt.innerHTML = input.value;
+	opt.appendChild(newOpt);
+	input.value = '';
+}
+
+function confirmInfo(){
+	let currentInfo = document.querySelector("#currentLevel");
+	let newInfo = document.querySelector("#lvl");
+	currentInfo.innerHTML = newInfo.value;
+}
+
+function resetInfo(){
+	let currentInfo = document.querySelector("#currentLevel");
+	let options = document.querySelector("#option");
+
+}
+
+function updateTime(){
+	let now = new Date();
+	let ampm = "AM";
+
+	h = now.getHours();
+	m = now.getMinutes();
+	s = now.getSeconds();
+	
+	if(h>= 12){
+		if(h>12) h -= 12;
+		ampm= 'PM';
+	}
+
+	if(m<10) m= '0'+m;
+	if(s<10) s= '0'+s;
+	let dateTime = now.toLocaleDateString()+ ' ' + h + ':' + m + ':' + s + ' ' + ampm;
+	let t = document.querySelector("#time");
+	let tVal = document.createElement("span");
+	tVal.innerHTML = dateTime;
+	t.appendChild(tVal);
+
+}
